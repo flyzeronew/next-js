9d28d4bc0fe0ae4a14cf47ba88498bde0daf6e48
flyzero <flyzeronew@gmail.com>
2022-10-03 23:35:54 +0800

    9

--- pages/[Post].js
+++ pages/[Post].js
@@ -3,7 +3,7 @@ import {useRouter} from 'next/router'
 import Image from 'next/image'
 import Navber from './comps/Navber'
 
-export default function Post({menu,portal_menu}) {
+export default function Post() {
   const router =useRouter();
   //return <pre>{JSON.stringify(menu,null,4)}</pre>
   //return <pre>{JSON.stringify(portal_menu,null,4)}</pre>
@@ -18,15 +18,15 @@ export default function Post({menu,portal_menu}) {
 
   )
 }
-Post.getInitialProps =async (i)=>{  
-    if(!i.req){return{menu:[]};} //防呆
-    const {query } =i;
-    const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.Post);
-    const res2 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/portal_menu');
-    const menu =await res1.json();
-    const portal_menu =await res2.json();
-    return{
-        menu:menu,
-        portal_menu:portal_menu.portal_menu
-    };
-}
+// Post.getInitialProps =async (i)=>{  
+//     if(!i.req){return{menu:[]};} //防呆
+//     const {query } =i;
+//     const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.Post);
+//     const res2 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/portal_menu');
+//     const menu =await res1.json();
+//     const portal_menu =await res2.json();
+//     return{
+//         menu:menu,
+//         portal_menu:portal_menu.portal_menu
+//     };
+// }
