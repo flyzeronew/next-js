c3265e91a53ad1b9e700b0c651e0703cba13ee4a
flyzero <flyzeronew@gmail.com>
2022-10-03 23:51:11 +0800

    22

--- pages/[Post].js
+++ pages/[Post].js
@@ -3,13 +3,14 @@ import {useRouter} from 'next/router'
 import Image from 'next/image'
 import Navber from './comps/Navber'
 
-export default function Post({menu,portal_menu}) {
+export default function post({menu,portal_menu}) {
   const router =useRouter();
   //return <pre>{JSON.stringify(menu,null,4)}</pre>
-  return <pre>{JSON.stringify(portal_menu,null,4)}</pre>
+  //return <pre>{JSON.stringify(portal_menu,null,4)}</pre>
   return (
 
     <div className="container">  
+    {menu.program.title}
     {/* <Navber menu={menu} portal_menu={portal_menu}/>   */}
        <div id="back">
           <div id="back-img1"><Image src="/gotop.png" alt="arraw" width={50} height={50} /></div>
@@ -18,10 +19,10 @@ export default function Post({menu,portal_menu}) {
 
   )
 }
-Post.getInitialProps =async (i)=>{  
+post.getInitialProps =async (i)=>{  
     if(!i.req){return{menu:[]};} //防呆
     const {query } =i;
-    const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.Post);
+    const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.post);
     const res2 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/portal_menu');
     const menu =await res1.json();
     const portal_menu =await res2.json();
