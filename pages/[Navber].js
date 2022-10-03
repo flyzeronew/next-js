607da5ac2f981a5896ec346d4b6d483ff39d7fb1
flyzero <flyzeronew@gmail.com>
2022-10-03 21:32:06 +0800

    更新

---
+++ pages/[Navber].js
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+import Link from "next/link"
+
+function Navber ({menu,portal_menu}) {    
+return(
+
+    <div className="program_header">
+     
+        <div className="main_header">
+            <div className="header_inner">
+                <h1 className="header_logo">
+                    <Link href="https://www.tvbs.com.tw/" >
+                        <a target="_blank">
+                            <Image src="/TVBS_logo.svg" alt="logo" width={102} height={49} />
+                        </a>
+                    </Link>                        
+                </h1>
+
+
+                <div className="header_search_pc">
+                    <input className="search_input" type="search" id="search" placeholder="關鍵字"/>
+                    <Link href="https://www.tvbs.com.tw/ours/en" >
+                        <a className="en_page"  target="_new">EN</a>
+                    </Link>                        
+                </div>
+
+                <div className="header_search_m">                      
+                    <div className="search_page">                                                  
+                        <div className="search_page_inner">
+                            <input className="search_page_input" type="search" id="search_mobile" placeholder="請輸入關鍵字"/>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+      
+            <nav>
+                <div className="nav_bg"></div>
+                <ul className="nav_inner_pc">
+                    {
+                        portal_menu.length == 0
+                        ? 'Loading menu...': portal_menu.map((val, index) => (                                
+                            <li key={index} >
+                            <a href={val.url}>{val.title}</a> 
+                                {
+                                    val.child ? 
+                                        <div className="nav_sub">
+                                            {
+                                                val.child.map(val2 => (
+                                                    <a href={val2.url}>{val2.title}</a>
+                                                ))
+                                            }                                            
+                                        </div>
+                                    : '' 
+                                }
+                            </li>                                
+                        ))
+                    } 
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    )
+}
+export default Navber;
--- pages/[Post].js
+++ pages/[Post].js
@@ -1,15 +1,18 @@
 
 import {useRouter} from 'next/router'
 import Image from 'next/image'
-import Header from './comps/Navber'
+import Header from "./[Navber].js"
 
 export default function Post({menu,portal_menu}) {
   const router =useRouter();
-//   return <pre>{JSON.stringify(menu,null,4)}</pre>
+  // 印資料
+  //return <pre>{JSON.stringify(menu,null,4)}</pre>
+  //return <pre>{JSON.stringify(portal_menu,null,4)}</pre>
   return (
 
-    <div className="container">  
-    <Header menu={menu} portal_menu={portal_menu}/>  
+    <div className="container">   
+    
+       <Header menu={menu} portal_menu={portal_menu}/>
        <div id="back">
           <div id="back-img1"><Image src="/gotop.png" alt="arraw" width={50} height={50} /></div>
        </div>        
