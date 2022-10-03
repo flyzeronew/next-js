dac3afa5fe6c3b19dff4d5ec9d0968a0dfa4fce7
flyzero <flyzeronew@gmail.com>
2022-10-03 19:30:26 +0800

    更新

--- pages/comps/Navber.js
+++ pages/comps/Navber.js
@@ -2,11 +2,11 @@ import Image from 'next/image'
 import Link from "next/link"
 
 export default function Navber ({menu,portal_menu}) {    
-console.log(menu);
+var title=menu.program.title
 return(
 
     <div className="program_header">
-
+{title}
     </div>
 
     )
