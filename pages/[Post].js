0488e34ead7d27eda141803e7ebeb0069185dec6
flyzero <flyzeronew@gmail.com>
2022-10-03 18:32:44 +0800

    推

---
+++ pages/[Post].js
@@ -0,0 +1,18 @@
+
+import {useRouter} from 'next/router'
+import Image from 'next/image'
+
+export default function Post() {
+  const router =useRouter();
+
+  return (
+
+    <div className="container">    
+       <div id="back">
+          <div id="back-img1"><Image src="/gotop.png" alt="arraw" width={50} height={50} /></div>
+       </div>        
+    </div>    
+
+  )
+}
+
