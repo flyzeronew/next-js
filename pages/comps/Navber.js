957d9c7067bb27a7e850c48a2f8ade501bdc3598
flyzero <flyzeronew@gmail.com>
2022-10-03 19:12:50 +0800

    222

--- pages/comps/Navber.js
+++ pages/comps/Navber.js
@@ -5,7 +5,25 @@ export default function Navber ({menu,portal_menu}) {
 return(
 
     <div className="program_header">
-
+{
+                        portal_menu.length == 0
+                        ? 'Loading menu...': portal_menu.map((val, key) => (                                
+                            <li key={key}>
+                            <a href={val.url}>{val.title}</a> 
+                                {
+                                    val.child ? 
+                                        <div className="nav_sub">
+                                            {
+                                                val.child.map(val2 => (
+                                                    <a href={val2.url}>{val2.title}</a>
+                                                ))
+                                            }                                            
+                                        </div>
+                                    : '' 
+                                }
+                            </li>                                
+                        ))
+                    } 
     </div>
 
     )
