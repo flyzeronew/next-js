d8b9410a577d7c0dc057df81444462b4e370af75
flyzero <flyzeronew@gmail.com>
2022-10-03 18:45:02 +0800

    測試

--- pages/[Post].js
+++ pages/[Post].js
@@ -2,9 +2,9 @@
 import {useRouter} from 'next/router'
 import Image from 'next/image'
 
-export default function Post() {
+export default function Post({menu,portal_menu}) {
   const router =useRouter();
-
+  return <pre>{JSON.stringify(menu,null,4)}</pre>
   return (
 
     <div className="container">    
@@ -15,4 +15,15 @@ export default function Post() {
 
   )
 }
-
+Post.getInitialProps =async (i)=>{  
+    if(!i.req){return{menu:[]};} //防呆
+    const {query } =i;
+    const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.Post);
+    const res2 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/portal_menu');
+    const menu =await res1.json();
+    const portal_menu =await res2.json();
+    return{
+        menu:menu,
+        portal_menu:portal_menu.portal_menu
+    };
+}
