bf1dd93487b55c1314db8be18a45f83d18cf73f7
flyzero <flyzeronew@gmail.com>
2022-10-03 19:24:49 +0800

    更新

--- pages/[Post].js
+++ pages/[Post].js
@@ -9,7 +9,7 @@ export default function Post({menu,portal_menu}) {
   return (
 
     <div className="container">  
-    <Header menu='aa' portal_menu='bb'/>  
+    <Header menu={menu} portal_menu={portal_menu}/>  
        <div id="back">
           <div id="back-img1"><Image src="/gotop.png" alt="arraw" width={50} height={50} /></div>
        </div>        
