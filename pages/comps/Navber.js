fb3b0bb05071dc753c3db737c3fbbf87f7eb47a3
flyzero <flyzeronew@gmail.com>
2022-10-03 19:27:26 +0800

    .3

--- pages/comps/Navber.js
+++ pages/comps/Navber.js
@@ -2,11 +2,11 @@ import Image from 'next/image'
 import Link from "next/link"
 
 export default function Navber ({menu,portal_menu}) {    
-
+console.log(menu);
 return(
 
     <div className="program_header">
-{menu}
+
     </div>
 
     )
