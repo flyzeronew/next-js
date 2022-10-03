75578014248379d6980d5fce2542c541c286e245
flyzero <flyzeronew@gmail.com>
2022-10-03 23:40:24 +0800

    89

--- pages/[Post].js
+++ pages/[Post].js
@@ -7,6 +7,7 @@ export default function Post() {
   const router =useRouter();
   return (
     <div className="container">  
+    111
        <div id="back">
           <div id="back-img1"><Image src="/gotop.png" alt="arraw" width={50} height={50} /></div>
        </div>        
