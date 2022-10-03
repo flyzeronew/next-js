7862c9151961b8afaafa6a0387cb62580a925dad
flyzero <flyzeronew@gmail.com>
2022-10-03 21:41:40 +0800

    1

--- pages/[Post].js
+++ pages/[Post].js
@@ -20,15 +20,15 @@ export default function Post({menu,portal_menu}) {
 
   )
 }
-// Post.getInitialProps =async (i)=>{  
-//     if(!i.req){return{menu:[]};} //防呆
-//     const {query } =i;
-//     const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.Post);
-//     const res2 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/portal_menu');
-//     const menu =await res1.json();
-//     const portal_menu =await res2.json();
-//     return{
-//         menu:menu,
-//         portal_menu:portal_menu.portal_menu
-//     };
-// }
+Post.getInitialProps =async (i)=>{  
+    if(!i.req){return{menu:[]};} //防呆
+    const {query } =i;
+    const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.Post);
+    const res2 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/portal_menu');
+    const menu =await res1.json();
+    const portal_menu =await res2.json();
+    return{
+        menu:menu,
+        portal_menu:portal_menu.portal_menu
+    };
+}
