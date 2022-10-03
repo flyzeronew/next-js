ae7494f661f4b5b0c7d4216b571a7017febcd50a
flyzero <flyzeronew@gmail.com>
2022-10-03 19:33:55 +0800

    測試

--- pages/comps/Navber.js
+++ pages/comps/Navber.js
@@ -2,11 +2,29 @@ import Image from 'next/image'
 import Link from "next/link"
 
 export default function Navber ({menu,portal_menu}) {    
-var title=menu.program.title
+
 return(
 
     <div className="program_header">
-{title}
+{
+                        portal_menu.length == 0
+                        ? 'Loading menu...': portal_menu.map((val, key) => (                                
+                            <li key={key}>
+                            <a href={val.url}>{val.title}</a> 
+                                {
+                                    val.child ? 
+                                        <div className="nav_sub">
+                                            {
+                                                val.child.map(val2 => (
+                                                    <a href={val2.url}>{val2.title}</a>
+                                                ))
+                                            }                                            
+                                        </div>
+                                    : '' 
+                                }
+                            </li>                                
+                        ))
+                    } 
     </div>
 
     )
