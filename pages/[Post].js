e10fa75e45516370258d0bb36c82b2d17d91590d
flyzero <flyzeronew@gmail.com>
2022-10-03 23:37:20 +0800

    8

--- pages/[Post].js
+++ pages/[Post].js
@@ -1,16 +1,12 @@
 
 import {useRouter} from 'next/router'
 import Image from 'next/image'
-import Navber from './comps/Navber'
+
 
 export default function Post() {
   const router =useRouter();
-  //return <pre>{JSON.stringify(menu,null,4)}</pre>
-  //return <pre>{JSON.stringify(portal_menu,null,4)}</pre>
   return (
-
     <div className="container">  
-    {/* <Navber menu={menu} portal_menu={portal_menu}/>   */}
        <div id="back">
           <div id="back-img1"><Image src="/gotop.png" alt="arraw" width={50} height={50} /></div>
        </div>        
@@ -18,15 +14,4 @@ export default function Post() {
 
   )
 }
-// Post.getInitialProps =async (i)=>{  
-//     if(!i.req){return{menu:[]};} //防呆
-//     const {query } =i;
-//     const res1 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/menu?id='+query.Post);
-//     const res2 = await fetch('https://2017tvbsapp-st.tvbs.com.tw/api3/news_program_api/portal_menu');
-//     const menu =await res1.json();
-//     const portal_menu =await res2.json();
-//     return{
-//         menu:menu,
-//         portal_menu:portal_menu.portal_menu
-//     };
-// }
+
