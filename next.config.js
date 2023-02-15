89f9c701a962a0e203acc4b0cef1354ae4b26450
flyzero <48467541+flyzeronew@users.noreply.github.com>
2023-02-15 11:13:22 +0800

    測試 圖騙網域

--- next.config.js
+++ next.config.js
@@ -3,12 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    domains: ['localhost'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cc.tvbs.com.tw',
-      },
+        hostname: 'img.youtube.com'
+      }
     ],
-  },
+  }, 
 }
 module.exports = nextConfig
